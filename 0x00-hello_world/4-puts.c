cca81bccc0ded6ebcae790e735bdd4cec58531d2
Collins Manase <collomanase@gmail.com>
2022-03-10 23:14:16 +0000

    Put followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * main - puts "Programming is like building a multilingual puzzle"
+ * main - puts "Programming is like building a multilingual puzzle
  * return 0 always (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
