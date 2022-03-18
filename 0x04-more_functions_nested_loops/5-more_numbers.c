41caca2853d457ec71a5e39e281179bc5aba2540
Collins Manase <collomanase@gmail.com>
2022-03-18 01:14:33 +0000

    print 10 times the numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,38 @@
+# include "main.h"
+
+/**
+ *more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ *Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+	int i, j, k, r;
+
+	i = 0;
+	r = 0;
+
+	while (i < 10)
+	{
+		while (r < 15)
+		{
+			if (r < 10)
+			{
+				k = r;
+			}
+
+			else
+			{
+				j = r / 10;
+				k = r % 10;
+				_putchar (j + '0');
+			}
+			_putchar (k + '0');
+			r++;
+		}
+		i++;
+		r = 0;
+		_putchar ('\n');
+	}
+}
