6266f7874a4b5fedaca7baa5333993d551e6196b
Collins Manase <collomanase@gmail.com>
2022-03-18 23:58:39 +0000

    print positive or negative integer

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - main
+ * main - prints if integer is positive or negative
  * Return: 0
  */
 
@@ -10,8 +10,6 @@ int main(void)
 	int i;
 
 	i = 0;
-
-
 	positive_or_negative(i);
 
 	return (0);
