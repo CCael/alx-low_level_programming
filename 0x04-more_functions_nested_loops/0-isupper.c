f82a6801d536af2d90842f3e7059bb1f194d2600
Collins Manase <collomanase@gmail.com>
2022-03-18 00:29:51 +0000

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase character
+ *
+ * @c: print int
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
