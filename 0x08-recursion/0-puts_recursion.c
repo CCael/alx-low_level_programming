06499d67ad033b37e4d918fe32d4a193e01567a4
Collins Manase <collomanase@gmail.com>
2022-03-30 22:40:15 +0000

    prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	else
+	if (*s != '\0')
 	{
 		_putchar(*(s + 0));
 		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('/n');
+		return;
 	}
 }
