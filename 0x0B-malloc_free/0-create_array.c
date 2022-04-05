22e80efd1d78427baef467f560eb47765aba0380
Collins Manase <collomanase@gmail.com>
2022-04-05 18:52:05 +0000

    creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,10 +19,9 @@ char *create_array(unsigned int size, char c);
 		return (NULL);
 	if (size != 0)
 	{
-		while (a = 0; a < size)
+		for (a = 0; a < size; a++)
 		{
 			*(arr + a) = c;
-			a++
 		}
 
 		return (arr);
