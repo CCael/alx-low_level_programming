10c8178a54bd28a0cd837daad8a8b972fa687eb7
Collins Manase <collomanase@gmail.com>
2022-04-05 22:46:55 +0000

    merges two strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *  * *_strdup - copies the string given as parameter
+ *   * @str: string to duplicate
+ *    *
+ *     * Return: pointer to the copied string (Success), NULL (Error)
+ *      */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	dup = malloc(i * sizeof(*dup) + 1);
+	if (dup == NULL)
+		return (NULL);
+
+	while (len < i)
+	{
+		dup[len] = str[len];
+		len++;
+	}
+
+	dup[len] = '\0';
+	return (dup);
+}
