6bc1752825f0dd8dffe9634c6c1c2a34c8a65b78
Collins Manase <collomanase@gmail.com>
2022-04-11 15:51:01 +0000

    unction that initialize a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,14 +6,4 @@
  *@d: pointer to struct dog to initialize
  *@name: name to initialize
  *@age: age to initialize
- *@owner: owner to initialize
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+ *@owner:
