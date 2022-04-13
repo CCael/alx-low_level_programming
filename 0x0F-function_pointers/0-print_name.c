fbf30b18e216e08259ab4e75839497ed8fb9a18c
Collins Manase <collomanase@gmail.com>
2022-04-13 19:45:25 +0000

    prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,16 @@
-#include <stdlib>
 #include "function_pointers.h"
 
 /**
  * print_name - prints a name
  * @name: name's main
  * @f: void function
+ * Return: no return
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name || !f)
+	if (name && f)
 	{
-		return;
+		f(name);
 	}
-
-	f(name);
 }
